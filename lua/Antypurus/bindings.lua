a6facaed8d3ed4ebe63057c55c52895717030f70
Tiago Magalhaes <tiago.magalhaes@ctw.bmwgroup.com>
2023-03-27 23:30:25 +0100

    language server configuration and further keybinds

--- lua/Antypurus/bindings.lua
+++ lua/Antypurus/bindings.lua
@@ -8,8 +8,30 @@ vim.keymap.set("n", "<c-s>", vim.cmd.write)
 vim.keymap.set("i", "<c-s>", vim.cmd.write)
 
 --window management
-vim.api.nvim_set_keymap("n", "<leader>cc", ":q<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<leader>cc", ":q<CR>", { noremap = true })
 
 -- reload nvim configurations
 local init_lua_path = os.getenv("MYVIMRC")
 vim.keymap.set("n", "<leader><c-r>", ":source " .. init_lua_path .. "<CR>")
+
+-- moving code up and down
+vim.api.nvim_set_keymap("v", "<A-Down>", ":m '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap("v", "<A-Up>", ":m '<-2<CR>gv=gv", { noremap = true })
+vim.keymap.set("n", "<A-Up>", "<Up>ddp<Up>")
+vim.keymap.set("n", "<A-Down>", "ddp")
+
+-- paste over token
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- format file
+vim.keymap.set("n", "<leader>ff", function()
+    vim.lsp.buf.format()
+end)
+
+-- replace word globally
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- source file
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
--- lua/Antypurus/init.lua
+++ lua/Antypurus/init.lua
@@ -1,3 +1,43 @@
 require 'Antypurus.bindings'
 require 'Antypurus.plugins'
 require 'Antypurus.color'
+
+-- line numbers
+vim.opt.nu = true
+-- relative line numbers in normal mode and absolute
+-- line numbers in edit mode, this should make things
+-- easier overall when i need the absolute line number
+vim.cmd [[
+    autocmd InsertEnter * :set norelativenumber
+    autocmd InsertLeave * :set relativenumber
+]]
+
+-- indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- line wrapping
+vim.opt.wrap = true
+
+-- long undos with undotree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- searching
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- colors
+vim.opt.termguicolors = true
+
+-- scrolloff
+vim.opt.scrolloff = 8
+
+-- update time
+vim.opt.updatetime = 50
+
