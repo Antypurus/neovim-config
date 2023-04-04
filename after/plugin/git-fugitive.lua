70a9ec67eecbd290fffb2dd04fbff2a227327ff4
Tiago Magalhaes <tiago.magalhaes@ctw.bmwgroup.com>
2023-04-04 19:11:32 +0100

    updated git commit binding

--- after/plugin/git-fugitive.lua
+++ after/plugin/git-fugitive.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
 vim.keymap.set("n", "<leader>gp", ":Git pull<CR>")
 vim.keymap.set("n", "<leader>gca", function()
     commit_message = vim.fn.input("Commit Message > ")
-    commit_command = ":Git commit -am \"" .. commit_message .. "\"<CR>"
+    commit_command = ":Git commit -am \"" .. commit_message .. "\""
     vim.cmd(commit_command)
 end)
 vim.keymap.set("n", "<leader>gps", ":Git push<CR>")
