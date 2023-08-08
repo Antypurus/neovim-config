e32bf07c026b13e601844841fdebfeb9b3a3a574
Tiago Magalhaes <antypurus@Tiagos-Mac-mini.home>
2023-08-08 20:18:34 +0100

    update

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,16 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {
+            "ThirdParty",
+            "build",
+        },
+        wrap_results = true,
+        prompt_prefix = "",
+        path_display = "smart"
+    },
+}
 
 vim.keymap.set('n', '<C-p>', function()
 	builtin.find_files({previewer=false})
