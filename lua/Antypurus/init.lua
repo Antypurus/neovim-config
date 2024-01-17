23819491e16fc9f5d7f4db236837baebcebeb06a
Tiago Magalhaes <tiago.magalhaes@ctw.bmwgroup.net>
2024-01-17 04:35:03 +0000

    removes current buffer highlighting for now

--- lua/Antypurus/init.lua
+++ lua/Antypurus/init.lua
@@ -19,9 +19,9 @@ vim.cmd [[ autocmd FileChangedShellPost *
   \ echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None ]]
 
 -- highlight current window/pane
-vim.cmd [[
-  autocmd WinEnter * setlocal winhighlight=Normal:ActiveWindow,NormalNC:InactiveWindow
-]]
+-- vim.cmd [[
+--   autocmd WinEnter * setlocal winhighlight=Normal:ActiveWindow,NormalNC:InactiveWindow
+-- ]]
 
 -- indentation
 vim.opt.tabstop = 4
