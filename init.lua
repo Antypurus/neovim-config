bd83067349319c02d2ec9a974122d372f7de03c5
Antypurus <tiagojsmagalhaes@gmail.com>
2026-03-18 22:38:09 +0000

    added keybinds for window split management

--- init.lua
+++ init.lua
@@ -63,7 +63,6 @@ vim.o.cursorline = true
 vim.o.scrolloff = 10
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
 
 -- Diagnostic Config & Keymaps
