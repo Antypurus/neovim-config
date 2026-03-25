baf4ba571934aaae0a7fd0450c0fca8436d53fef
Antypurus <tiagojsmagalhaes@gmail.com>
2026-03-25 05:27:00 +0000

    initial seperation of mega init.lua done, still need to plug everything together and add some missing functionality

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -7,6 +7,26 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 
+local lazy_ui = {
+	-- If you are using a Nerd Font: set icons to an empty table which will use the
+	-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+	icons = vim.g.have_nerd_font and {} or {
+		cmd = "⌘",
+		config = "🛠",
+		event = "📅",
+		ft = "📂",
+		init = "⚙",
+		keys = "🗝",
+		plugin = "🔌",
+		runtime = "💻",
+		require = "🌙",
+		source = "📄",
+		start = "🚀",
+		task = "📌",
+		lazy = "💤 ",
+	},
+}
+
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
@@ -32,8 +52,14 @@ require("lazy").setup({
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
 	},
+	{ "nvim-treesitter/nvim-treesitter" },
+	{ "nvim-mini/mini.nvim" },
 	{ "stevearc/conform.nvim" },
 	{ "saghen/blink.cmp", dependencies = {
 		"L3MON4D3/LuaSnip",
 	} },
-})
+	{ "folke/todo-comments.nvim" },
+
+	-- themes
+	{ "folke/tokyonight.nvim" },
+}, lazy_ui)
